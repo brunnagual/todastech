b22e7470a2ab7a2f02b0c1c7db816734c5977bf3
brunnagual <brunnagual@gmail.com>
2023-07-31 20:30:14 -0300

    Polimorfismo e Generics

---
+++ DART/aula05/02generics.dart
@@ -0,0 +1,51 @@
+//  T pode ser usado quando não sabemos qual tipo de dados iremos usar.
+class Segmento<T> {
+  T? produto;
+
+  Segmento(T produto) {
+    this.produto = produto;
+  }
+
+  void exibir() {
+    if (this.produto is Vestuario) {
+      print("Objeto do tipo Vestuário");
+    }
+
+    if (this.produto is Smartphones) {
+      print("Objeto do tipo Smartphone");
+    }
+  }
+}
+
+class Vestuario {
+  String? produto;
+  String? tamanho;
+
+  Vestuario(String produto, String tamanho) {
+    this.produto = produto;
+    this.tamanho = tamanho;
+  }
+}
+
+class Smartphones {
+  String? produto;
+  String? processador;
+  String? sistema;
+
+  Smartphones(String produto, String processador, String sistema) {
+    this.produto = produto;
+    this.processador = processador;
+    this.sistema = sistema;
+  }
+}
+
+void main() {
+  Vestuario v = new Vestuario("Camisa Azul", "M");
+  Smartphones s = new Smartphones("iPhone 14", "M1", "iOS");
+
+  Segmento<Vestuario> obj1 = new Segmento<Vestuario>(v);
+  obj1.exibir();
+
+  Segmento<Smartphones> obj2 = new Segmento<Smartphones>(s);
+  obj2.exibir();
+}
